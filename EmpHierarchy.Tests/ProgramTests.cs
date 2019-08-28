9b09447e0e06ab509c37757dcf95e90f23e80a94
Eric Rukungu <Erukungu@abnosoftwares.co.ke>
2019-08-28 09:32:45 +0300

    implement test cases on the program

--- EmpHierarchy.Tests/ProgramTests.cs
+++ EmpHierarchy.Tests/ProgramTests.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using NUnit.Framework;
 
 namespace EmpHierarchy.Tests
@@ -6,7 +8,7 @@ namespace EmpHierarchy.Tests
     public class ProgramTests
     {
         private readonly EmpHierarchyProgram _program;
-        private List<Employee> employees;
+        private List<Employee> _employees;
         public ProgramTests(string csv)
         {
             _program = new EmpHierarchyProgram(csv);
@@ -15,13 +17,51 @@ namespace EmpHierarchy.Tests
         [SetUp]
         public void Setup()
         {
-            employees = _program.ValidEmployees;
+            _employees = _program.ValidEmployees;
         }
 
         [Test]
-        public void Test1()
+        public void Salaries_are_valid_integer_numbers()
         {
-            Assert.Pass();
+            var count = _employees.Count(e => !IsValidInteger(e.Salary.ToString()));
+            Assert.Equals(count, 0);
         }
+
+        [Test]
+        public void Only_one_ceo()
+        {
+            var count = _employees.Count(e => string.IsNullOrWhiteSpace(e.ManagerId));
+            Assert.Equals(count, 1);
+        }
+
+        [Test]
+        public void All_managers_are_employees()
+        {
+            var managers = _employees.Select(e => e.ManagerId).ToList();
+            var count = _employees.Count(e => _employees.Any(l => managers.Contains(e.EmployeeId)));
+            Assert.Equals(count, managers.Count);
+        }
+
+        [Test]
+        public void Employee_reports_to_one_manager()
+        {
+            var duplicates = _employees
+                .GroupBy(e => e.EmployeeId)
+                .Where(g => g.Count() > 1)
+                .ToList();
+            Assert.Equals(duplicates.Count, 0);
+        }
+
+        #region HelperMethods
+
+        private static bool IsValidInteger(string number)
+        {
+            var isValid = int.TryParse(number, out var validResult);
+            Console.WriteLine($" {number} output {validResult} ");
+            return isValid;
+        }
+
+
+        #endregion
     }
 }
